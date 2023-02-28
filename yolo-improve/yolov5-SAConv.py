74777ab6ffd9e93703dada7e74ec13db32ab67bb
z1069614715 <1069614715@qq.com>
2023-02-28 22:36:41 +0800

    add-sac

---
+++ yolo-improve/yolov5-SAConv.py
@@ -0,0 +1,144 @@
+class ConvAWS2d(nn.Conv2d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        self.register_buffer('weight_gamma', torch.ones(self.out_channels, 1, 1, 1))
+        self.register_buffer('weight_beta', torch.zeros(self.out_channels, 1, 1, 1))
+
+    def _get_weight(self, weight):
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
+                                  keepdim=True).mean(dim=3, keepdim=True)
+        weight = weight - weight_mean
+        std = torch.sqrt(weight.view(weight.size(0), -1).var(dim=1) + 1e-5).view(-1, 1, 1, 1)
+        weight = weight / std
+        weight = self.weight_gamma * weight + self.weight_beta
+        return weight
+
+    def forward(self, x):
+        weight = self._get_weight(self.weight)
+        return super()._conv_forward(x, weight, None)
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        self.weight_gamma.data.fill_(-1)
+        super()._load_from_state_dict(state_dict, prefix, local_metadata, strict,
+                                      missing_keys, unexpected_keys, error_msgs)
+        if self.weight_gamma.data.mean() > 0:
+            return
+        weight = self.weight.data
+        weight_mean = weight.data.mean(dim=1, keepdim=True).mean(dim=2,
+                                       keepdim=True).mean(dim=3, keepdim=True)
+        self.weight_beta.data.copy_(weight_mean)
+        std = torch.sqrt(weight.view(weight.size(0), -1).var(dim=1) + 1e-5).view(-1, 1, 1, 1)
+        self.weight_gamma.data.copy_(std)
+    
+class SAConv2d(ConvAWS2d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 s=1,
+                 p=None,
+                 g=1,
+                 d=1,
+                 act=True,
+                 bias=True):
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=s,
+            padding=autopad(kernel_size, p, d),
+            dilation=d,
+            groups=g,
+            bias=bias)
+        self.switch = torch.nn.Conv2d(
+            self.in_channels,
+            1,
+            kernel_size=1,
+            stride=s,
+            bias=True)
+        self.switch.weight.data.fill_(0)
+        self.switch.bias.data.fill_(1)
+        self.weight_diff = torch.nn.Parameter(torch.Tensor(self.weight.size()))
+        self.weight_diff.data.zero_()
+        self.pre_context = torch.nn.Conv2d(
+            self.in_channels,
+            self.in_channels,
+            kernel_size=1,
+            bias=True)
+        self.pre_context.weight.data.fill_(0)
+        self.pre_context.bias.data.fill_(0)
+        self.post_context = torch.nn.Conv2d(
+            self.out_channels,
+            self.out_channels,
+            kernel_size=1,
+            bias=True)
+        self.post_context.weight.data.fill_(0)
+        self.post_context.bias.data.fill_(0)
+        
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = Conv.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        # pre-context
+        avg_x = torch.nn.functional.adaptive_avg_pool2d(x, output_size=1)
+        avg_x = self.pre_context(avg_x)
+        avg_x = avg_x.expand_as(x)
+        x = x + avg_x
+        # switch
+        avg_x = torch.nn.functional.pad(x, pad=(2, 2, 2, 2), mode="reflect")
+        avg_x = torch.nn.functional.avg_pool2d(avg_x, kernel_size=5, stride=1, padding=0)
+        switch = self.switch(avg_x)
+        # sac
+        weight = self._get_weight(self.weight)
+        out_s = super()._conv_forward(x, weight, None)
+        ori_p = self.padding
+        ori_d = self.dilation
+        self.padding = tuple(3 * p for p in self.padding)
+        self.dilation = tuple(3 * d for d in self.dilation)
+        weight = weight + self.weight_diff
+        out_l = super()._conv_forward(x, weight, None)
+        out = switch * out_s + (1 - switch) * out_l
+        self.padding = ori_p
+        self.dilation = ori_d
+        # post-context
+        avg_x = torch.nn.functional.adaptive_avg_pool2d(out, output_size=1)
+        avg_x = self.post_context(avg_x)
+        avg_x = avg_x.expand_as(out)
+        out = out + avg_x
+        return self.act(self.bn(out))
+
+class Bottleneck_SAC(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = SAConv2d(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class C3_SAC(C3):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(Bottleneck_SAC(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
