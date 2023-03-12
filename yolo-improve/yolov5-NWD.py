9b80fed673edeef5e2abeb227fbe78a3867a9f74
z1069614715 <1069614715@qq.com>
2023-03-12 23:27:04 +0800

    update-yolov5-nwd

--- yolo-improve/yolov5-NWD.py
+++ yolo-improve/yolov5-NWD.py
@@ -32,7 +32,7 @@ def wasserstein_loss(pred, target, eps=1e-7, constant=12.8):
 
 nwd = wasserstein_loss(pbox, tbox[i]).squeeze()
 iou_ratio = 0.5
-lbox += iou_ratio * (1.0 - nwd).mean() + (1 - iou_ratio) * (1.0 - iou).mean()  # iou loss
+lbox += (1 - iou_ratio) * (1.0 - nwd).mean() + iou_ratio * (1.0 - iou).mean()  # iou loss
 
 # Objectness
 iou = (iou.detach() * iou_ratio + nwd.detach() * (1 - iou_ratio)).clamp(0, 1).type(tobj.dtype)
