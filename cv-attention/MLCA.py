f95b2f8f1e8aaceaad02f27cb010f0a75e75bcf3
z1069614715 <1069614715@qq.com>
2023-11-30 20:38:25 +0800

    add-MLCA

---
+++ cv-attention/MLCA.py
@@ -0,0 +1,49 @@
+import math
+from torch import nn
+import torch.nn.functional as F
+
+class MLCA(nn.Module):
+    def __init__(self, in_size, local_size=5, gamma = 2, b = 1,local_weight=0.5):
+        super(MLCA, self).__init__()
+
+        # ECA 计算方法
+        self.local_size=local_size
+        self.gamma = gamma
+        self.b = b
+        t = int(abs(math.log(in_size, 2) + self.b) / self.gamma)   # eca  gamma=2
+        k = t if t % 2 else t + 1
+
+        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=(k - 1) // 2, bias=False)
+        self.conv_local = nn.Conv1d(1, 1, kernel_size=k, padding=(k - 1) // 2, bias=False)
+
+        self.local_weight=local_weight
+
+        self.local_arv_pool = nn.AdaptiveAvgPool2d(local_size)
+        self.global_arv_pool=nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        local_arv=self.local_arv_pool(x)
+        global_arv=self.global_arv_pool(local_arv)
+
+        b,c,m,n = x.shape
+        b_local, c_local, m_local, n_local = local_arv.shape
+
+        # (b,c,local_size,local_size) -> (b,c,local_size*local_size)-> (b,local_size*local_size,c)-> (b,1,local_size*local_size*c)
+        temp_local= local_arv.view(b, c_local, -1).transpose(-1, -2).reshape(b, 1, -1)
+        temp_global = global_arv.view(b, c, -1).transpose(-1, -2)
+
+        y_local = self.conv_local(temp_local)
+        y_global = self.conv(temp_global)
+
+
+        # (b,c,local_size,local_size) <- (b,c,local_size*local_size)<-(b,local_size*local_size,c) <- (b,1,local_size*local_size*c)
+        y_local_transpose=y_local.reshape(b, self.local_size * self.local_size,c).transpose(-1,-2).view(b,c, self.local_size , self.local_size)
+        y_global_transpose = y_global.view(b, -1).transpose(-1, -2).unsqueeze(-1)
+
+        # 反池化
+        att_local = y_local_transpose.sigmoid()
+        att_global = F.adaptive_avg_pool2d(y_global_transpose.sigmoid(),[self.local_size, self.local_size])
+        att_all = F.adaptive_avg_pool2d(att_global*(1-self.local_weight)+(att_local*self.local_weight), [m, n])
+
+        x = x * att_all
+        return x
--- cv-attention/readme.md
+++ cv-attention/readme.md
@@ -32,6 +32,7 @@
 | EMA | True | https://arxiv.org/abs/2305.13563v2 |
 | CloAttention | True | https://arxiv.org/abs/2303.17803 |
 | LSKBlock | True | https://arxiv.org/pdf/2303.09030.pdf |
+| MLCA | True | https://www.sciencedirect.com/science/article/pii/S0952197623006267 |
 
 # Install
 安装命令:pip install timm einops efficientnet_pytorch -i https://pypi.tuna.tsinghua.edu.cn/simple
@@ -39,7 +40,7 @@
 # Course
 1. [yolov5添加注意力哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1s84y1775U) [yolov5添加注意力-补充事项-哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1hG4y1M71X)
 2. [yolov7添加注意力哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1pd4y1H7BK)
-3. [yolov8添加注意力哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1dv4y167rF)
+3. [yolov8添加注意力哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1ZQ4y1J7oC/) [yolov8添加注意力进阶版哔哩哔哩视频教学链接](https://www.bilibili.com/video/BV1ZQ4y1J7oC/)
 
 # Reference
 https://github.com/xmu-xiaoma666/External-Attention-pytorch  
@@ -48,3 +49,4 @@ https://github.com/rayleizhu/BiFormer
 https://github.com/XiaLiPKU/EMANet  
 https://github.com/qhfan/CloFormer/tree/main
 https://github.com/zcablii/LSKNet
+https://github.com/wandahangFY/MLCA
